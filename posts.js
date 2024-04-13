097d66953d03a7aa90660d80426e5ec05f10f235
signal86 <diafakemail@gmail.com>
2024-04-13 02:15:39 -0500

    incomplete posts functions

---
+++ posts.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongo = require("mongodb");
+const app = express();
+
+const newPost = (content) => async () => {
+    const client = new mongo.MongoClient(process.env.uri);
+
+    const db = client.db("town");   
+
+    const posts = db.collection("posts");
+
+    posts.insertOne({
+        content: content
+    });
+};
+
+const reviewPosts = async () => {
+    const client = new mongo.MongoClient(process.env.uri);
+
+    const db = client.db("town");
+
+    const posts = db.collection("posts");
+
+    const poop = await posts.find().toArray();
+
+    console.log(poop);
+};
+
+newPost("test");
+
+reviewPosts();
+
+const PORT = process.env.PORT || 6969;
+
+app.use(express.static("./"));
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
