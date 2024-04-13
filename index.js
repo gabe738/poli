41664060818b6310b7be38ac0f38bf920d7e44dc
logan <adsfov@gmail.com>
2024-04-13 01:11:50 -0500

    mongo outline (db test)

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const mongo = require("mongodb");
 const app = express();
 
+(async () => {
+    const client = new mongo.MongoClient(process.env.uri);
+
+    const db = client.db("town");   
+
+    const users = db.collection("users");
+
+    // name, username, email, password (hashed), phone number, dob
+    const user = await users.findOne({
+        username: "gay"
+    })
+
+    console.log(user);
+})()
+
 const PORT = process.env.PORT || 6969;
 
 app.use(express.static("./public"));
