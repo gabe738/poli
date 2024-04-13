2bec2acf500378b2c6b836d17d53c77efceda3a3
signal86 <diafakemail@gmail.com>
2024-04-13 03:01:58 -0500

    posts functionality

--- posts.js
+++ posts.js
@@ -1,25 +1,31 @@
 const express = require("express");
 const mongo = require("mongodb");
+const timeManager = require("./timeManager.js");
 const app = express();
-const formattedTime = require("./timeManager.js");
 
-const newPost = (contentIn) => async () => {
+const newPost = async (authorIdIn, contentIn, isCommentIn) => {
     const client = new mongo.MongoClient(process.env.uri);
 
     await client.connect();
 
     const db = client.db("town");   
     const posts = db.collection("posts");
+    const users = db.collection("users");
 
-    const time = formattedTime.getTime();
+    const author = await users.findOne({_id: authorIdIn});
 
     await posts.insertOne({
+        authorId: authorIdIn,
         content: contentIn,
-        time: time
+        time: timeManager.getTime(),
+        city: author.city,
+        isComment: isCommentIn
     });
+
+    client.close();
 };
 
-const reviewPosts = async () => {
+const retrieveAllPosts = async () => {
     const client = new mongo.MongoClient(process.env.uri);
 
     await client.connect();
@@ -30,14 +36,42 @@ const reviewPosts = async () => {
 
     const poop = await posts.find().toArray();
 
-    console.log(poop);
+    client.close();
+    return poop;
 };
 
-newPost("test");
+const deletePost = async (postId) => {
+    const client = new mongo.MongoClient(process.env.uri);
 
-reviewPosts();
+    await client.connect();
 
-// console.log(formattedTime.getTime())
+    const db = client.db("town");
+
+    const posts = db.collection("posts");
+
+    await posts.deleteOne({_id: postId});
+
+    client.close();
+}
+
+// const temp = async (postId) => {
+//     const client = new mongo.MongoClient(process.env.uri);
+
+//     await client.connect();
+
+//     const db = client.db("town");
+
+//     const posts = db.collection("posts");
+//     const users = db.collection("users");
+
+//     const a = await users.findOne({test: "fart123"});
+
+//     console.log(a.password);
+
+//     client.close();
+// }
+
+// temp();
 
 const PORT = process.env.PORT || 6969;
 
