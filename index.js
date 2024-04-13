93dc1eddb019eaf12f4f3b6356f74ade85156d5d
logan <adsfov@gmail.com>
2024-04-13 01:05:32 -0500

    style main page, get mongo working

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const express = require("express");
 const mongo = require("mongodb");
-
-const client = new mongo.MongoClient(process.env.uri);
-
-(async () => {
-    await client.connect();
-
-    const db = client.db("db");
-
-    db.createCollection("users");
-})()
-
 const app = express();
+
 const PORT = process.env.PORT || 6969;
 
 app.use(express.static("./public"));
-
-console.log(process.env.uri)
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
