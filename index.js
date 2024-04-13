3d67e16581884262ba87f46fc5a892d47e1e8f24
N-magic <n8martin2025@gmail.com>
2024-04-13 17:43:37 -0500

    Commented through the main index.js, the file that we use to host

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
+// All the imports, Express, MongoDB(for storing users), crypto for data safety, path for routing
 const express = require("express");
 const mongo = require("mongodb");
-const crypto = require("crypto");
+const crypto = require("crypto"); // Not crypto currency, but instead protecting user data with encryption
 const path = require("path");
 const app = express();
 
-
+//Set up the connection to MongoDB using our API key in .env
 const client = new mongo.MongoClient(process.env.uri);
-
 const db = client.db("town");   
-
 const users = db.collection("users");
 const posts = db.collection("posts");
 
 const PORT = process.env.PORT || 6969;
 
+// Some rendering code, more boilerplate effectively
 app.set("view engine", "ejs");
 app.use(express.json());
 
+// Gotta prevent that cross-site scripting
 const sanitize = data => {
     return escape(data.replaceAll(/(<|>|\/|"|'|`|\\)/g, "")).trim(); // improve fr
 }
 
+// Following this is what connects the front end to the back end
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/main/index.html"));
+    res.sendFile(path.join(__dirname, "./views/main/index.html")); // Fetches main site
 })
 
 app.get("/signup", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/createAccount/index.html"));
+    res.sendFile(path.join(__dirname, "./views/createAccount/index.html")); // Fetches account creation site
 })
 
-app.post("/signup", async (req, res) => {
+app.post("/signup", async (req, res) => { // The method to signup users
     const name = sanitize(req.body.name);
     const username = sanitize(req.body.username);
     const password = sanitize(req.body.password);
@@ -42,23 +44,24 @@ app.post("/signup", async (req, res) => {
         return;
     }
 
-    if (password != confirmed_password) {
-        res.status(400).send("uh oh. youre so skibidi, on toilet!!!!!");
+    if (password != confirmed_password) { // Alert users if passwords to not match
+        res.status(400).send("The passwords do not match, please try again.");
         return;
     }
 
 
-    const passHash = crypto.createHash("sha256");
+    const passHash = crypto.createHash("sha256"); // Creates a hashing method that allows us to save passwords securely
     passHash.update(password);
 
+
+    // Code to check and see if someone with that username already exists
     const foundUser = await users.findOne({ username_lower: username.toLowerCase() })
-    
     if (foundUser) {
-        res.status(400).send("uh ohh.. A SKbidi user with that name ALREADY EXISTS. on toilet bro..a"); // bad request
+        res.status(400).send("A user already exists with this username: Consider using a different username; or trying to log in with that username."); // error notification
         return;
     }
 
-    const userCookie = crypto.randomUUID();
+    const userCookie = crypto.randomUUID(); // Temporary cookie that allows people to not have to re-log in, and to validate posts
 
     users.insertOne({
         name: name,
@@ -71,17 +74,17 @@ app.post("/signup", async (req, res) => {
     })
 
     res.status(200).json({
-        cookie: userCookie
+        cookie: userCookie // Saves our temporary log in cookie in local storage
     });
 
-    console.log("user created:", username);
+    console.log("user created:", username); // Tells us it works
 })
 
 app.post("/login", (req, res) => {
     const username = sanitize(req.body.username);
     const password = sanitize(req.body.password);
 
-    if (!username || !password) {
+    if (!username || !password) { // There wasn't a username or password
         res.sendStatus(400); // bad request
         return;
     }
@@ -89,14 +92,14 @@ app.post("/login", (req, res) => {
     const passHash = crypto.createHash("sha256");
     passHash.update(password);
     
-    const foundUser = users.findOne({ username_lower: username.toLowerCase() })
+    const foundUser = users.findOne({ username_lower: username.toLowerCase() }) // Checks for a user with that name
     
-    if (!foundUser) {
+    if (!foundUser) { // When it can't find a user
         res.sendStatus(400); // bad request
         return;
     }
 
-    if (foundUser.password_hash != passHash.digest("hex")) {
+    if (foundUser.password_hash != passHash.digest("hex")) { // The incorrect password was used
         res.sendStatus(401); // unauthorized
         return;
     }
@@ -104,8 +107,10 @@ app.post("/login", (req, res) => {
     // successfully loged in !!! (need ot code)
 })
 
-app.get("*", (req, res) => {
+
+
+app.get("*", (req, res) => { // Our 404 page so we don't show a blank error
     res.sendFile(path.join(__dirname, "./404.html"));
 })
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`)); // Boilerplate
